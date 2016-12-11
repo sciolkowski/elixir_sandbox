9d6049076fc251b63a99ea346f3fa28812f0fc97
sciolkowski <szymon.ciolkowski@eengine.pl>
2016-12-11 14:58:13 +0100

    Get RGB color of identicon

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -6,6 +6,7 @@ defmodule Identicon do
   def main(input) do
       input
       |> hash_input
+      |> pick_color
   end
 
   @doc """
@@ -16,6 +17,11 @@ defmodule Identicon do
     |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
+    [r, g, b]
   end
 
 end
