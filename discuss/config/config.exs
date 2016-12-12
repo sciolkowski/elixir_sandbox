2669c86fa1adc1dabfb24f84fdd54aadcaa1e3a5
sciolkowski <szymon.ciolkowski@eengine.pl>
2016-12-12 22:06:53 +0100

    Add Phoeinx app

---
+++ discuss/config/config.exs
@@ -0,0 +1,28 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :discuss,
+  ecto_repos: [Discuss.Repo]
+
+# Configures the endpoint
+config :discuss, Discuss.Endpoint,
+  # url: [host: "localhost"],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
+  secret_key_base: "j5F/t8r8NMXc6flzkH/Fe7GBXHpYZSh9wzLALrhn9LIl8zpqc6qYzLRMzPYEXaGa",
+  render_errors: [view: Discuss.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Discuss.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
