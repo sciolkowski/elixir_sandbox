d1f0174230f4a12a2fd3d9139e77bad3fb9278a2
sciolkowski <szymon.ciolkowski@eengine.pl>
2016-12-13 21:20:18 +0100

    Create Topic controller and db migration

---
+++ discuss/web/controllers/topic_controller.ex
@@ -0,0 +1,9 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+
+  def new(conn, params) do
+    # The `conn` struct is a total focal point of how Phoenix works.
+    # It comes with request, and then is passed from function to function
+    # until it's ready to be returned to the user as a response (somewhat :)
+  end
+end
