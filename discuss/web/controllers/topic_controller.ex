f3951d8ac4c50595661a20f57a5169efc98429fa
sciolkowski <szymon.ciolkowski@eengine.pl>
2016-12-14 21:16:25 +0100

    Form for creating Topics with errors support

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -1,9 +1,24 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
+  # With alias we do not have to type whole Discuss.Topic everytime, only Topic.
+  alias Discuss.Topic
 
-  def new(conn, params) do
+  def new(conn, _params) do
     # The `conn` struct is a total focal point of how Phoenix works.
     # It comes with request, and then is passed from function to function
     # until it's ready to be returned to the user as a response (somewhat :)
+    changeset = Topic.changeset(%Topic{}, %{})
+    render conn, "new.html", changeset: changeset
   end
+
+  def create(conn, %{"topic" => topic} = params) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+      end
+  end
+
 end
